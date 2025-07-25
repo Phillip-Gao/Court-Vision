2975909029a7d49f9e66a45ee1093e6332791c52
Phillip-Gao <87874752+Phillip-Gao@users.noreply.github.com>
2025-07-25 14:54:59 -0400

    detection - update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from utils.video_utils import read_video, save_video
 def main():
 
     # read video
-    frames = read_video('input_video.mp4')
+    frames = read_video('input_videos/video_1.mp4')
 
     # save video
-    save_video(frames, 'output_video.avi')
+    save_video(frames, 'output_videos/output_video.avi')
 
 if __name__ == "__main__":
     main()
